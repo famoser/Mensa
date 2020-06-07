12b2b0e16e1a8c39291cb29e2d025a6d4eefc71a
Florian Moser <git@famoser.ch>
2020-06-07 21:42:51 +0200

    remove serialization adapter when not needed

--- app/src/main/java/ch/famoser/mensa/models/Location.kt
+++ app/src/main/java/ch/famoser/mensa/models/Location.kt
@@ -1,9 +1,5 @@
 package ch.famoser.mensa.models
 
-import com.squareup.moshi.JsonClass
-
-
-@JsonClass(generateAdapter = true)
 data class Location(val title: String, val mensas: List<Mensa>) {
     init {
         for (mensa in mensas) {
