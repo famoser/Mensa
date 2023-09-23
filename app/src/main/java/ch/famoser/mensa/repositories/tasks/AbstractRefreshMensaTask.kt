d75580b19fa6644458f30abf4ff66aecfb44f366
Florian Moser <git@famoser.ch>
2023-09-23 11:06:09 +0200

    maint: Update dependencies

--- app/src/main/java/ch/famoser/mensa/repositories/tasks/AbstractRefreshMensaTask.kt
+++ app/src/main/java/ch/famoser/mensa/repositories/tasks/AbstractRefreshMensaTask.kt
@@ -11,16 +11,19 @@ abstract class AbstractRefreshMensaTask<TArgument> : AsyncTask<TArgument, Int, U
 
     private val asyncTaskId = UUID.randomUUID()
 
+    @Deprecated("Deprecated in Java")
     override fun onPreExecute() {
         super.onPreExecute()
         EventBus.getDefault().post(RefreshMensaStartedEvent(asyncTaskId))
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onPostExecute(result: Unit?) {
         super.onPostExecute(result)
         EventBus.getDefault().post(RefreshMensaFinishedEvent(asyncTaskId))
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onProgressUpdate(vararg values: Int?) {
         super.onProgressUpdate(*values)
         EventBus.getDefault().post(RefreshMensaProgressEvent(asyncTaskId, values[0]!!, values[1]!!))
